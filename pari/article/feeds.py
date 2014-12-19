0c66aae7a23104a031331ff88233fa8440974d77
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2014-12-19 15:04:55 +0530

    Module level to method level variable change.

--- pari/article/feeds.py
+++ pari/article/feeds.py
@@ -12,10 +12,12 @@ from ..news.models import NewsPost
 import itertools
 import datetime
 
-days_ago = int(settings.FEED_GENERATION_DAYS)
-
 
 class BaseFeed(Feed):
+    def __init__(self, *args, **kwargs):
+        self.days_ago = int(settings.FEED_GENERATION_DAYS)
+        super(BaseFeed, self).__init__(*args, **kwargs)
+
     def __call__(self, request, *args, **kwargs):
         accept_header = request.META.get("HTTP_ACCEPT", "")
         if accept_header.find("application/atom+xml") >= 0:
@@ -44,10 +46,14 @@ class BaseFeed(Feed):
 class AllFeed(BaseFeed):
     title = "PARI consolidated feed"
     link = "/feeds/all/"
-    description = "Updates on the PARI site over the past {0} days".format(days_ago)
+
+    def __init__(self, *args, **kwargs):
+        super(AllFeed, self).__init__(*args, **kwargs)
+        self.description = ("Updates on the PARI site over the "
+                            "past {0} days".format(self.days_ago))
 
     def items(self):
-        x_days_ago = timezone.now() - datetime.timedelta(days=days_ago)
+        x_days_ago = timezone.now() - datetime.timedelta(days=self.days_ago)
         return itertools.chain(
             Article.objects.filter(publish_date__gte=x_days_ago),
             Album.objects.filter(publish_date__gte=x_days_ago),
@@ -61,58 +67,82 @@ class AllFeed(BaseFeed):
 class ArticleFeed(BaseFeed):
     title = "PARI article feed"
     link = "/feeds/articles/"
-    description = "Article updates on the PARI site over the past {0} days".format(days_ago)
+
+    def __init__(self, *args, **kwargs):
+        super(ArticleFeed, self).__init__(*args, **kwargs)
+        self.description = ("Article updates on the PARI site "
+                            "over the past {0} days".format(self.days_ago))
 
     def items(self):
-        x_days_ago = timezone.now() - datetime.timedelta(days=days_ago)
+        x_days_ago = timezone.now() - datetime.timedelta(days=self.days_ago)
         return Article.objects.filter(publish_date__gte=x_days_ago)
 
 
 class AlbumFeed(BaseFeed):
     title = "PARI album feed"
     link = "/feeds/albums/"
-    description = "Album updates on the PARI site over the past {0} days".format(days_ago)
+
+    def __init__(self, *args, **kwargs):
+        super(AlbumFeed, self).__init__(*args, **kwargs)
+        self.description = ("Album updates on the PARI site "
+                            "over the past {0} days".format(self.days_ago))
 
     def items(self):
-        x_days_ago = timezone.now() - datetime.timedelta(days=days_ago)
+        x_days_ago = timezone.now() - datetime.timedelta(days=self.days_ago)
         return Album.objects.filter(publish_date__gte=x_days_ago)
 
 
 class FaceFeed(BaseFeed):
     title = "PARI face feed"
     link = "/feeds/faces/"
-    description = "Face updates on the PARI site over the past {0} days".format(days_ago)
+
+    def __init__(self, *args, **kwargs):
+        super(FaceFeed, self).__init__(*args, **kwargs)
+        self.description = ("Face updates on the PARI site "
+                            "over the past {0} days".format(self.days_ago))
 
     def items(self):
-        x_days_ago = timezone.now() - datetime.timedelta(days=days_ago)
+        x_days_ago = timezone.now() - datetime.timedelta(days=self.days_ago)
         return Face.objects.filter(publish_date__gte=x_days_ago)
 
 
 class ResourceFeed(BaseFeed):
     title = "PARI resource feed"
     link = "/feeds/resources/"
-    description = "Resource updates on the PARI site over the past {0} days".format(days_ago)
+
+    def __init__(self, *args, **kwargs):
+        super(ResourceFeed, self).__init__(*args, **kwargs)
+        self.description = ("Resource updates on the PARI site over the "
+                            "past {0} days".format(self.days_ago))
 
     def items(self):
-        x_days_ago = timezone.now() - datetime.timedelta(days=days_ago)
+        x_days_ago = timezone.now() - datetime.timedelta(days=self.days_ago)
         return Resource.objects.filter(publish_date__gte=x_days_ago)
 
 
 class FactoidFeed(BaseFeed):
     title = "PARI factoid feed"
     link = "/feeds/factoids/"
-    description = "Factoid updates on the PARI site over the past {0} days".format(days_ago)
+
+    def __init__(self, *args, **kwargs):
+        super(FactoidFeed, self).__init__(*args, **kwargs)
+        self.description = ("Factoid updates on the PARI site over the "
+                            "past {0} days".format(self.days_ago))
 
     def items(self):
-        x_days_ago = timezone.now() - datetime.timedelta(days=days_ago)
+        x_days_ago = timezone.now() - datetime.timedelta(days=self.days_ago)
         return Factoid.objects.filter(publish_date__gte=x_days_ago)
 
 
 class NewsPostFeed(BaseFeed):
     title = "PARI news feed"
     link = "/feeds/newsposts/"
-    description = "News updates on the PARI site over the past {0} days".format(days_ago)
+
+    def __init__(self, *args, **kwargs):
+        super(NewsPostFeed, self).__init__(*args, **kwargs)
+        self.description = ("News updates on the PARI site over the "
+                            "past {0} days".format(self.days_ago))
 
     def items(self):
-        x_days_ago = timezone.now() - datetime.timedelta(days=days_ago)
+        x_days_ago = timezone.now() - datetime.timedelta(days=self.days_ago)
         return NewsPost.objects.filter(publish_date__gte=x_days_ago)
